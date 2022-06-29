599ad39c1b1e132941c968f37ee949b4ca78a62a
admin <wiraisha1oemrawsingh@gmail.com>
2022-06-29 12:01:18 -0700

    update the project with changes to pick a plan

--- vue-project/app.js
+++ vue-project/app.js
@@ -1,18 +1,33 @@
-Vue.createApp({
+let PlanComponent={
+    template: '#plan-template',
+    props: {
+        name: { type: String, required: true },
+        selected: {type:Boolean, default:false}
+    },
+    methods:{
+        select(){
+            this.$emit('select', this.name)
+        }
+    }
+}
 
+let PlanPickerComponent={
+    components:{plan:PlanComponent},
+    template:'#plan-picker-template',
+    data() {
+        return {
+            plans: ['The Single', 'The Curious', 'The Addict'],
+            selectedPlan:null
+        }
+    },
+    methods:{
+        selectPlan(plan){
+            this.selectedPlan=plan
+        }
+    }
+}
+
+const app=Vue.createApp({
+    components:{PlanPicker: PlanPickerComponent }
 })
-    .component('plan-picker',{
-        template:'#plan-picker-template',
-        data() {
-            return {
-                plans: ['The Single', 'The Curious', 'The Addict']
-            }
-        }
-    })
-    .component('plan', {
-        template: '#plan-template',
-        props: {
-            name: { type: String, required: true }
-        }
-    })
-    .mount('#app') 
+.mount('#app') 
